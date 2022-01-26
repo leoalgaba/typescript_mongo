46d9f33565ec02756d4119ca8c4b83d609b71a17
leoalgaba <leo_algaba@hotmail.com>
2022-01-26 18:54:05 +0100

    arreglo dist

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = exports.HOST = exports.NODE_ENV = void 0;
-exports.NODE_ENV = process.env.NODE_ENV || 'development';
-exports.HOST = process.env.HOST || '127.0.0.1';
-exports.PORT = process.env.PORT || '8000';
