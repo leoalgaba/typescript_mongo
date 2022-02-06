0348473c78f1fce672c91305d700bd2193ead705
leoalgaba <leo_algaba@hotmail.com>
2022-02-06 10:08:35 +0100

    variables locales en mongo
    Conectamos una variable de entorno en mongo.

--- src/models/database.ts
+++ src/models/database.ts
@@ -1,9 +1,10 @@
 import { connect } from "mongoose"
 import chalk from 'chalk'
+import 'dotenv/config'
 
 export const startconnect = async (): Promise<void> => {
     try {
-        const db = await connect("mongodb://localhost/crud-database")
+        const db = await connect(`${process.env.MONGO_URI}`)
         console.log(`La base de datos ${ chalk.green (db.connection.name) } esta conectada localmente`)
     } catch (err) {
         console.error()
